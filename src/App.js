96e46572fdb42bfd1a821f6101afadcb3d5fa473
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:34:43 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState } from 'react';
+
+
+
+function App(props) {
+
+ const[description,setDescription]= useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className="conteiner">
+    <div className="head">{props.data.Name +'   '+ props.data.SurName}</div>
+
+    <div className="mainBody">
+        <div className="picture"> <img src={props.data.image} alt={props.data.id}/></div>
+        <div className="description">
+            <p><span>Имя:</span> {props.data.Name}</p>
+            <p><span>Фамилия:</span> {props.data.SurName}</p>
+            <p><span>Возраст:</span> {props.data.fullYearsOld}</p>
+            <p><span>Семейное положение:</span> {props.data.family}</p>
+            <p><span>Cтрана:</span> {props.data.country}</p>
+            <p><span>Тел.:</span> {props.data.tell}</p>
+            <p><span>Email:</span> {props.data.email}</p>
+            <p className="lsDesc">{props.data.description}</p>
+           
+        </div>
     </div>
+    <div className="footer">
+        <button 
+            onClick={()=>setDescription(prev =>!prev)}
+            >{description? 'Cкрыть':'Подробнее..'}</button>
+            {description && <p>{props.data.allDescription}</p>}
+    </div>
+    
+ </div>
+   
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,20 @@
+import React from "react";
+import myImage from '../src/img/1234.jpg';
+
+
+export const cards=
+    {
+        id:1,
+        Name:'Evgeniy',
+        SurName:'Kovalsky',
+        yearOfBorn:'25.05.1983',
+        country:'Ukraine',
+        fullYearsOld:'39',
+        family:'женат',
+        tell:'+38 067 611 85 56',
+        email:'evgeniykovalsky04388@gmail.com',
+        description:' Возможность командировок: да Водительское удостоверение: есть.Собственный автомобиль: есть. Вредные привычки отсутствуют.',
+        allDescription:'Работоспособность, целеустремленность, стрессоустойчивость, умение работать в команде, дисциплинированность, аккуратность.',
+        image:myImage
+        
+    }
